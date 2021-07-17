f77e1a39c0b7ec3b4a15ae45599af201008d4857
Barbara Whiting <whitingba@gmail.com>
2021-07-16 21:07:05 -0600

    working on adding carousel for projects

---
+++ js/carousel.js
@@ -0,0 +1,32 @@
+// console.log('hello');
+
+// $(document).ready(function () {
+//   // Activate Carousel
+//   $("#myCarousel").carousel();
+
+//   // Enable Carousel Indicators
+//   $(".item1").click(function () {
+//     $("#myCarousel").carousel(0);
+//   });
+//   $(".item2").click(function () {
+//     $("#myCarousel").carousel(1);
+//   });
+//   $(".item3").click(function () {
+//     $("#myCarousel").carousel(2);
+//   });
+//   $(".item4").click(function () {
+//     $("#myCarousel").carousel(3);
+//   });
+
+//   // Enable Carousel Controls
+//   $(".left").click(function () {
+//     $("#myCarousel").carousel("prev");
+//   });
+//   $(".right").click(function () {
+//     $("#myCarousel").carousel("next");
+//   });
+// });
+
+var myCarousel = document.querySelector('#carouselExampleSlidesOnly');
+var carousel = new bootstrap.Carousel(carouselExampleSlidesOnly);
+
