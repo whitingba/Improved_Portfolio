e5f15bba11fc05f8809dfca1e5f3e5c426254ee0
Barbara Whiting <whitingba@gmail.com>
2021-08-02 21:22:09 -0600

    add indicators & caption to carousel AND finish styling my certification collapse

---
+++ js/tabs.js
@@ -0,0 +1,26 @@
+function openCert(evt, certName) {
+  //variable declarations
+  var i, tabcontent, tablinks;
+
+  //Get all elements where class = "tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  //Grab all elements where class="tablinks" and remove the class "active
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  //Show the current tab and add an "active" class to the link that opened the tab
+
+  document.getElementById(certName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+
+}
+
+
+document.getElementById("defaultOpen").click();
