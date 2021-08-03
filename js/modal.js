75397b30dc648c190531fc77563456ec98428f9b
Barbara Whiting <whitingba@gmail.com>
2021-08-02 21:44:37 -0600

    add modal to view certifications

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,25 @@
-//get Modal
+// Get the modal
 var modal = document.getElementById("myModal");
 
-//get 1st image
-var img = document.getElementById("myImg");
-//grab modal image id
-var modalImg = document.getElementById("img01");
-//grab modal caption id
-var captionText = document.getElementById("caption");
-//create a function that onclick of the image a modal will display
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-//get the span element that will close the modal
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-//when user clicks on the span 'x', the modal will close
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
